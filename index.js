acefe5e9ce2d12c245873a5e24cb844b0105bb0a
Sunthon Rod <47098991+kunbunya@users.noreply.github.com>
2023-08-25 14:31:01 +0700

    Create index.js
    first quiz commit

---
+++ index.js
@@ -0,0 +1,66 @@
+let array = [3, 2, 5, 1, 4];
+let lists = [
+  { name: "Jojo", id: 40000 },
+  { name: "Prayut", id: 8400 },
+  { name: "banglee", id: 69 },
+  { name: "Meow", id: 9 },
+  { name: "Oreo", id: 10 },
+];
+
+//Q1 : Write code something from "array" to result [5,4,3,2,1] finally
+let res1 = (array) => {
+  let result = undefined;
+  //write code something
+
+  // end write code something
+  return result;
+};
+
+//Q2 : Write code something from "array" to result [1,2,3,4,5] finally
+let res2 = (array) => {
+  let result = undefined;
+  //write code something
+
+  // end write code something
+  return result;
+};
+
+//Q3 : Write code for select "name" obj from least id
+let res3 = (lists) => {
+  let result = undefined;
+  //write code something
+
+  // end write code something
+  return result;
+};
+
+//Q4 : Write code for select "name" obj from most id
+let res4 = (lists) => {
+  let result = undefined;
+  //write code something
+
+  // end write code something
+  return result;
+};
+
+/////////////////////////////////////////////////////////
+//check Q1
+console.log(
+  "5,4,3,2,1" == res1(array) ? "Pass ✅" : " false ❌ ",
+  "Your Result= " + res1(array)
+);
+//check Q2
+console.log(
+  "1,2,3,4,5" == res2(array) ? "Pass ✅" : " false ❌ ",
+  "Your Result= " + res2(array)
+);
+//check Q3
+console.log(
+  "Meow" == res3(lists) ? "Pass ✅" : " false ❌ ",
+  "Your Result= " + res3(lists)
+);
+//check Q4
+console.log(
+  "Jojo" == res4(lists) ? "Pass ✅" : " false ❌ ",
+  "Your Result= " + res4(lists)
+);
